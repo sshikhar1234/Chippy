ff34d1b64c4b2078e9ae63d9b16af7fe62c6c6c2
sshikhar1234 <sshikharshah@gmail.com>
2019-10-23 15:16:58 -0400

    Added player ability to shoot the enemy core. Added player and enemy health stats. Added Game Over Game Win screens.

--- app/src/main/java/project/university/chippy/GameEngine.java
+++ app/src/main/java/project/university/chippy/GameEngine.java
@@ -1,6 +1,7 @@
 package project.university.chippy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -29,7 +30,7 @@ public class GameEngine extends SurfaceView implements Runnable {
     int screenWidth;
     float mouseX;
     float mouseY;
-
+    Context context;
     String playerTapped = "";
 
     // game state
@@ -51,13 +52,17 @@ public class GameEngine extends SurfaceView implements Runnable {
     Bitmap roboArmHorizontalLight;
     Bitmap bgGame;
     Bitmap laserBitmap;
+    int playerHealth=100;
+    int enemyHealth=100;
+    boolean playerEnemyCollision = false;
+
     public GameEngine(Context context, int w, int h) {
         super(context);
         this.holder = this.getHolder();
         this.paintbrush = new Paint();
         this.screenWidth = w;
         this.screenHeight = h;
-
+this.context = context;
         //Setting the background
 
         this.bgGame = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_bg);
@@ -77,7 +82,12 @@ public class GameEngine extends SurfaceView implements Runnable {
         enemy = new Enemy((int) (screenWidth * 0.65), 400);
         enemy.setImage(BitmapFactory.decodeResource(this.getContext().getResources(),
                 R.drawable.ic_robo_resized));
-        enemy.setHitbox(new Rect((int) (screenWidth * 0.65), 400, enemy.getImage().getWidth() + (int) (screenWidth * 0.65), enemy.getImage().getHeight() + 350));
+        enemy.setHitbox(
+                new Rect(
+                        ((int) (screenWidth * 0.65)),
+                        400,
+                        enemy.getImage().getWidth() + (int) (screenWidth * 0.65),
+                        400+ enemy.getImage().getHeight()));
 
 
         roboArmHorizontal = BitmapFactory.decodeResource(this.getContext().getResources(),
@@ -188,11 +198,9 @@ public class GameEngine extends SurfaceView implements Runnable {
         Bullet newBullet = new Bullet(this.player.getxPos() + this.player.getImage().getWidth()+10,
                 this.player.getyPos() + this.player.getImage().getHeight() / 2,laserBitmap);
         bullets.add(newBullet);
-//      player.addBullet();
         paintbrush.setColor(Color.BLUE);
         paintbrush.setStyle(Paint.Style.FILL);
         paintbrush.setStrokeWidth(5);
-//        bullets = player.getBullets();
     }
     // ------------------------------
     // GAME ENGINE FUNCTIONS
@@ -200,8 +208,23 @@ public class GameEngine extends SurfaceView implements Runnable {
     // ------------------------------
 
     public void updatePositions() {
+
+
+        //Check Game OVer and Game Win Condition
+
+        if(playerHealth == 0){
+            //Game OVer
+            context.startActivity(new Intent(context,GameOverActivity.class));
+        }
+
+
         movePlayer(player.getImage(), this.mouseX, this.mouseY);
 
+        if(player.getHitbox().intersect(enemy.getHitbox()))
+        {
+            playerHealth = playerHealth - 1;
+            Log.d(TAG, "updatePositions: Over over");
+        }
         //make the bullet move
         if (bullets.size() > 0) {
             for (Bullet bullet : bullets) {
@@ -210,34 +233,74 @@ public class GameEngine extends SurfaceView implements Runnable {
                 currentHitbox.left = currentHitbox.left +35;
                 currentHitbox.right = currentHitbox.right +35;
                 bullet.setHibox(currentHitbox);
-                for(EnemyLeg currentLeg: enemyLegs){
 
-                    //TODO: Handle the HealthPoints Logic for each piece of Enemy
-                    //1. If the health is less than 50 then Change the image
-                    if(currentLeg.getHitPoints()<=50) {
-                        currentLeg.setImage(roboArmHorizontalLight);
+                //TODO: Check if the bullets are hitting enemy core then enemy dies!
+                //Player Wins
+                if(bullet.getHibox().intersect(this.enemy.getHitbox()))
+                {
+                    if(enemyHealth>0){
+                        enemyHealth = enemyHealth - 20;
                     }
-                    //2. If the health is less than 0 then remove it from the screem
-                    if(currentLeg.getHitPoints()<=0) {
-                        currentLeg.setxPos(-100);
-                        currentLeg.setyPos(-100);
-                        Rect legHitbox = currentLeg.getHitbox();
-                        legHitbox.left = -100;
-                        legHitbox.right = -100;
+                    else
+                    {
+                        Log.d(TAG, "updatePositions: BOOM");
+                        enemy.setxPos(-100);
+                        enemy.setyPos(-100);
+                        for(EnemyLeg currentLeg: enemyLegs) {
+                            currentLeg.setxPos(-100);
+                            currentLeg.setyPos(-100);
+                            Rect legHitbox = currentLeg.getHitbox();
+                            legHitbox.left = -100;
+                            legHitbox.right = -100;
+                            Rect enemyHitBox = new Rect(-100,-100,-100,-100);
+                            this.enemy.setHitbox(enemyHitBox);
+                            context.startActivity(new Intent(context,GameWinAcitivity.class));
+                        }
                     }
 
-                    //TODO: Check Collision between LASER and ENEMY
-                    if(bullet.getHibox().intersect(currentLeg.getHitbox())){
-                       currentLeg.setHitPoints(currentLeg.getHitPoints()-10);
-                        bullet.setxPos(-100);
-                        bullet.setyPos(-100);
-                        currentHitbox.left = -100;
-                        currentHitbox.right = -100;
-                        currentHitbox.top = -100;
-                        currentHitbox.bottom = -100;
-                        Log.d(TAG, "updatePositions: BOOM!");
+                }
+                else
+                {
+                    for(EnemyLeg currentLeg: enemyLegs){
+
+                        //TODO: Handle the HealthPoints Logic for each piece of Enemy
+                        //1. If the health is less than 50 then Change the image
+                        if(currentLeg.getHitPoints()<=50) {
+                            currentLeg.setImage(roboArmHorizontalLight);
+                        }
+                        //2. If the health is less than 0 then remove it from the screem
+                        if(currentLeg.getHitPoints()<=0) {
+                            currentLeg.setxPos(-100);
+                            currentLeg.setyPos(-100);
+                            Rect legHitbox = currentLeg.getHitbox();
+                            legHitbox.left = -100;
+                            legHitbox.right = -100;
+
+                        }
+
+                        //TODO: Check Collision between LASER and ENEMY
+                        if(bullet.getHibox().intersect(currentLeg.getHitbox())){
+                            if(this.player.getHitbox().intersect(currentLeg.getHitbox()))
+                            {
+                                playerEnemyCollision = true;
+                            }
+                            currentLeg.setHitPoints(currentLeg.getHitPoints()-10);
+                            bullet.setxPos(-100);
+                            bullet.setyPos(-100);
+                            currentHitbox.left = -100;
+                            currentHitbox.right = -100;
+                            currentHitbox.top = -100;
+                            currentHitbox.bottom = -100;
+
+                            //IF the enemy leg is hit by bullet, decrease enemy health by 1
+                            if(enemyHealth>20&&currentLeg.getHitPoints()==0) {
+                                Log.d(TAG, "updatePositions: Health Decreased");
+                                enemyHealth = enemyHealth - 1;
+                            }
+                        }
                     }
                 }
+
             }
         }
         //TODO: Check Collision between enemy and player's bullets
@@ -263,6 +326,18 @@ public class GameEngine extends SurfaceView implements Runnable {
             //Draw the enemy ship
             canvas.drawBitmap(enemy.getImage(), enemy.getxPos(), enemy.getyPos(), paintbrush);
             canvas.drawRect(this.enemy.getHitbox(), paintbrush);
+
+            Paint paintHealth = new Paint();
+            paintHealth.setTextSize(40);
+            paintHealth.setColor(Color.WHITE);
+            //Draw the player and enemy health stats
+            canvas.drawText("Player Health: "+playerHealth,100,100,paintHealth);
+            if(enemyHealth>0){
+                canvas.drawText("Enemy Health: "+enemyHealth,(int)(screenWidth*.70),100,paintHealth);
+            }
+            paintbrush.setColor(Color.TRANSPARENT);
+            canvas.drawRect(this.player.getHitbox(), paintbrush);
+            paintbrush.setColor(Color.BLUE);
 
             //Draw Enemy Protective Layer
             for (EnemyLeg enemyLeg : enemyLegs) {
